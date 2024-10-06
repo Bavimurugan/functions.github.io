8f8eae24b0c1b766c21129afea6b820b4f6bc315
Bavimurugan <bavimurugan23@gmail.com>
2024-10-06 20:00:22 +0530

    Added fun-expression.js

---
+++ fun-expression.js
@@ -0,0 +1,20 @@
+// Function Expressions
+
+console.log('****Function Expressions******');
+
+var firstdayFE = function() {
+    console.log('types of functions');
+}
+
+welcomeFE();
+
+let firstdayclass = function(name) {
+    console.log(`Welcome ${name}! to Javascript Tutorial`);
+}
+
+const addFE = function(firstnumber,secondnumber) {
+    return firstnumber+secondnumber;
+}
+
+welcomeYouFE('welcome all');
+addFE(7,6);
